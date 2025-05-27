e627f4a666f7be64f978948f9c169f60f2d0839a
Riddhi Dhingra <riddhidhingra1@gmail.com>
2025-05-28 02:18:10 +0530

    DAY1 COMPLETED

---
+++ 01basics/01datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+//treats javascript code as newer version
+//alert(3+3)  we are using node js not browser
+console.log(3+3)
+console.log("ridhi")
+
+//datatypes
+let name="rid"
+let age=19
+let isloggedin=true
+
+
+/*
+typeof can be used to get the datatype
+//primitive 
+number=2 to the power 53
+big int
+symbol=unique used in react figma etc.
+null =standalone value
+undefined
+string=""
+boolean=false/true
+
+//objects
+
+
+*/
---
+++ 01basics/01variables.js
@@ -0,0 +1,15 @@
+const account_id=5
+let account_name="riddhi"
+var account_email="riddhidhingr1@gmail.com"
+account_city="saharanpur"
+// account_id=2
+/*
+let-block scope
+var-functional scope
+use let 
+if we do not write the value in let element it takes it as undefined 
+*/
+account_email="rid@ri.com"
+account_name="siddhi"
+account_city="delhi"
+console.table([account_id,account_email,account_name,account_city])
